00f2ee873575bf34b03e68e3de3b9316fea3db1d
Pavel Karateev <lancelote.du.lac@gmail.com>
2016-12-06 21:27:35 +0000

    refactoring

--- utils/utils.py
+++ utils/utils.py
@@ -9,6 +9,7 @@ language = 'python'
 
 access_key = API_KEY
 
+
 def get_kata_data(slug, access_key=API_KEY):
     url = 'https://www.codewars.com/api/v1/code-challenges/%s' % slug
     params = {'access_key': access_key}
@@ -20,13 +21,14 @@ def get_kata_data(slug, access_key=API_KEY):
     else:
         raise Exception(response)
 
+
 def create_files(slug):
     fname = '{}.py'.format(slug.replace('-', '_'))  
     if os.path.isfile(fname):
         print('Error... Files already exists...')
     else:
-        sys.stdout = open(fname, 'w')
-        sys.stdout = open('test_' + fname, 'w')
+        with open(fname, 'w'), open('test_' + fname, 'w'):
+            pass
 
 
 # for key, value in data.items():
