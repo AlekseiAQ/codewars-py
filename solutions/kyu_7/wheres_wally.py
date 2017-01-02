0ab069a59d62cc732f6d8d1f9e896fe97372e72f
alekseyaq <kuznetz.vrn@gmail.com>
2017-01-02 00:55:04 -0500

    fix pylint error

--- solutions/kyu_7/wheres_wally.py
+++ solutions/kyu_7/wheres_wally.py
@@ -29,5 +29,5 @@ import re
 
 
 def wheres_wally(string):
-    match = re.compile('(^|.*[\s])(Wally)([\.,\s\']|$)').match(string)
+    match = re.compile(r'(^|.*[\s])(Wally)([\.,\s\']|$)').match(string)
     return match.start(2) if match else -1
