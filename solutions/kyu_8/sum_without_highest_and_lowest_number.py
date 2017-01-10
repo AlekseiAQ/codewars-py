71601905f90bcadfc6ccee610216de3521342e5e
alekseyaq <kuznetz.vrn@gmail.com>
2017-01-10 12:04:21 -0500

    fix pylint error

--- solutions/kyu_8/sum_without_highest_and_lowest_number.py
+++ solutions/kyu_8/sum_without_highest_and_lowest_number.py
@@ -29,6 +29,6 @@ I have created other katas. Have a look if you like coding and challenges.
 
 
 def sum_array(arr):
-    if arr == None or len(arr) < 3:
+    if arr is None or len(arr) < 3:
         return 0
     return sum(arr) - max(arr) - min(arr)
